2a4e81ef798d556d9e81b8a3448b59d4e6f43813
longyanyue <451028552@qq.com>
2015-05-04 15:25:10 +1000

    update2

--- DataProcess.R
+++ DataProcess.R
@@ -1,41 +1,86 @@
 require(caTools)
-plot(1:dim(dataset_pH)[1],dataset_pH$pH, type="l", ylab="pH(-)",xlab="",lwd=2, cex.lab=1,col="black",cex=1)
-trendline = runquantile(dataset_pH$pH, 1440, probs=c(0.40, 0.6))
-lines(1:dim(dataset_pH)[1],trendline[,1],col="red")
-lines(1:dim(dataset_pH)[1],trendline[,2],col="blue")
-i <- 51
-limit<-dim(dataset_pH)[1]
+
+
+#####real-time trendline build
+highermark <- 0.7
+lowermark <- 0.3
+windowsize <- 1440
+datalength <- dim(dataset_pH)[1]
 upbounds<<-0
 lowbounds<<-0
-eventimpact<-100
-while(i<=limit){
-  if(as.numeric(dataset_pH$pH[i]) > trendline[i,2]){
-    rate<-abs(as.numeric(dataset_pH$pH[i])-trendline[i,2])/trendline[i,2]
-    upbounds<-upbounds+20*rate
-    lowbounds<-lowbounds-20*rate
+m_upbounds<<-1.2
+m_lowbounds<<-0.8
+c_upbounds<<-1.4
+c_lowbounds<<-0.6
+m_weight <<- 5
+c_weight <<- 10
+eventimpact<-300
+
+upthreshold <- 8
+lowthreshold <- 6
+i <- 1
+trendline <- matrix(, nrow = datalength, ncol = 2)
+eventmarks <- numeric(datalength)
+trendlinestack <- dataset_pH$pH[1:windowsize]
+trendline[1:windowsize,] <- runquantile(trendlinestack, windowsize, probs=c(lowermark, highermark))
+while((i+windowsize)<=datalength){
+  temp_trendline <- runquantile(trendlinestack, windowsize, probs=c(lowermark, highermark))
+#####event detection process
+  if(as.numeric(dataset_pH$pH[i+windowsize]) > trendline[i+windowsize-1,2] && as.numeric(dataset_pH$pH[i+windowsize]) > upthreshold){
+    rate<-abs(as.numeric(dataset_pH$pH[i+windowsize])-trendline[i+windowsize-1,2])/trendline[i+windowsize-1,2]
+    upbounds<-upbounds+10*rate
+    lowbounds<-lowbounds-10*rate
+    if(as.numeric(dataset_pH$pH[i+windowsize]) > m_upbounds*trendline[i+windowsize-1,2]){
+      upbounds<-upbounds+m_weight
+      lowbounds<-lowbounds-m_weight
+    }
+    if(as.numeric(dataset_pH$pH[i+windowsize]) > c_upbounds*trendline[i+windowsize-1,2]){
+      upbounds<-upbounds+c_weight
+      lowbounds<-lowbounds-c_weight
+    }
     if(lowbounds<0)
       lowbounds<-0
   }
-  else if(as.numeric(dataset_pH$pH[i]) < trendline[i,1]){
-    rate<-abs(as.numeric(dataset_pH$pH[i])-trendline[i,1])/trendline[i,1]
+  else if(as.numeric(dataset_pH$pH[i+windowsize]) < trendline[i+windowsize-1,1] && as.numeric(dataset_pH$pH[i+windowsize]) < lowthreshold){
+    rate<-abs(as.numeric(dataset_pH$pH[i+windowsize])-trendline[i+windowsize-1,1])/trendline[i+windowsize-1,1]
     lowbounds<-lowbounds+20*rate
     upbounds<-upbounds-20*rate
+    if(as.numeric(dataset_pH$pH[i+windowsize]) < m_lowbounds*trendline[i+windowsize-1,1]){
+      upbounds<-upbounds-m_weight
+      lowbounds<-lowbounds+m_weight
+    }
+    if(as.numeric(dataset_pH$pH[i+windowsize]) < c_lowbounds*trendline[i+windowsize-1,1]){
+      upbounds<-upbounds-c_weight
+      lowbounds<-lowbounds+c_weight
+    }
     if(upbounds<0)
       upbounds<-0
   }
   else{
-    lowbounds<-lowbounds-80
-    upbounds<-upbounds-80
+    lowbounds<-lowbounds-100
+    upbounds<-upbounds-100
     if(lowbounds<0)
       lowbounds<-0
     if(upbounds<0)
       upbounds<-0
   }
   if(upbounds>=eventimpact){
-    points(i-50,as.numeric(dataset_pH$pH[i-50]),col="yellow")
+    eventmarks[i+windowsize] <- 1
   }
   else if(lowbounds>=eventimpact){
-    points(i-50,as.numeric(dataset_pH$pH[i-50]),col="green")
+    eventmarks[i+windowsize] <- -1
   }
-  i<-i+1
+  trendline[i+windowsize,] <- temp_trendline[windowsize,]
+  if(eventmarks[i+windowsize]==0){
+    trendlinestack[1:(windowsize-1)] <- trendlinestack[2:windowsize]
+    trendlinestack[windowsize] <- dataset_pH$pH[i+windowsize]
+  }
+    
+#  else
+#    trendline[i+windowsize,] <- c(mean(as.numeric(temp_trendline[(1:windowsize),1])),mean(as.numeric(temp_trendline[(1:windowsize),2])))
+  i <- i+1
 }
+
+
+
+
--- PDFoutput.R
+++ PDFoutput.R
@@ -11,18 +11,7 @@ i <- 1
 ##set window for plot for each page of PDF file, currently set to 1770 as this is the average points sampled per day in the current system. 
 ### there should be 1440 samples a day, but bug in current Delphi and hardware and it samples 1770 times a day on average. THIS NEEDS TO BE FIXED!!!!!
 
-inc <- 1440
-
-limit<-dim(dataset_pH)[1]
-upbounds<<-0
-lowbounds<<-0
-m_upbounds<<-1.2
-m_lowbounds<<-0.8
-c_upbounds<<-1.4
-c_lowbounds<<-0.6
-m_weight <<- 5
-c_weight <<- 10
-eventimpact<-200
+inc <- 1440*7
 
 data.end <- dim(dataset_pH)[1]  ##change this to the length of the data set or where you want to plotting to stop.
 
@@ -33,56 +22,13 @@ while(i+inc <= (data.end)){
   plot(sub,dataset_pH$pH[sub], type="l", ylab="pH(-)",xlab="",lwd=2, cex.lab=1,col="black",cex=1)
   lines(sub,trendline[sub,1],col="red")
   lines(sub,trendline[sub,2],col="blue")
-  j<-i
-  while(j<=(i+inc)){
-    if(as.numeric(dataset_pH$pH[j]) > trendline[j,2]){
-      rate<-abs(as.numeric(dataset_pH$pH[j])-trendline[j,2])/trendline[j,2]
-      upbounds<-upbounds+20*rate
-      lowbounds<-lowbounds-20*rate
-      if(as.numeric(dataset_pH$pH[j]) > m_upbounds*trendline[j,2]){
-        upbounds<-upbounds+m_weight
-        lowbounds<-lowbounds-m_weight
-      }
-      if(as.numeric(dataset_pH$pH[j]) > c_upbounds*trendline[j,2]){
-        upbounds<-upbounds+c_weight
-        lowbounds<-lowbounds-c_weight
-      }
-      if(lowbounds<0)
-        lowbounds<-0
-    }
-    else if(as.numeric(dataset_pH$pH[j]) < trendline[j,1]){
-      rate<-abs(as.numeric(dataset_pH$pH[j])-trendline[j,1])/trendline[j,1]
-      lowbounds<-lowbounds+20*rate
-      upbounds<-upbounds-20*rate
-      if(as.numeric(dataset_pH$pH[j]) < m_lowbounds*trendline[j,1]){
-        upbounds<-upbounds-m_weight
-        lowbounds<-lowbounds+m_weight
-      }
-      if(as.numeric(dataset_pH$pH[j]) < c_lowbounds*trendline[j,1]){
-        upbounds<-upbounds-c_weight
-        lowbounds<-lowbounds+c_weight
-      }
-      if(upbounds<0)
-        upbounds<-0
-    }
-    else{
-      lowbounds<-lowbounds-100
-      upbounds<-upbounds-100
-      if(lowbounds<0)
-        lowbounds<-0
-      if(upbounds<0)
-        upbounds<-0
-    }
-    if(upbounds>=eventimpact){
-      points(j,as.numeric(dataset_pH$pH[j]),col="yellow")
-    }
-    else if(lowbounds>=eventimpact){
-      points(j,as.numeric(dataset_pH$pH[j]),col="green")
-    }
-    j<-j+1
+
+  for(j in sub){
+    if(eventmarks[j]==1)
+      points(j,dataset_pH$pH[j],col="yellow")
+    else if(eventmarks[j]==-1)
+      points(j,dataset_pH$pH[j],col="green")
   }
-  
-  
   i <- i+ inc
 }
 
